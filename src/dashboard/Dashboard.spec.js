33bd94b713898fdcd895d0ae8df059223dd65920
squashgray <squashgray@gmail.com>
2019-12-16 20:04:07 -0800

    mvp?

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
-// Test away
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"; // for toBeInTheDocument to work
+import Dashboard from "./Dashboard";
+
+test("should match snapshot", () => {
+  const wrapper = render(<Dashboard />);
+  expect(wrapper.asFragment()).toMatchSnapshot(); // elements are rendering correctly //
+});
+
+test("defaults to unlocked and open", () => {
+  const { getByText } = render(<Dashboard />);
+  const unlocked = getByText(/unlocked/i);
+  const open = getByText(/open/i);
+
+  expect(unlocked).toBeInTheDocument();
+  expect(open).toBeInTheDocument(); //finds unlocked and open on the dashboard //
+});
